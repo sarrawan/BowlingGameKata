ccf34d976ab234219ea94b30a0ec94a5448deec8
RawanS <rawansar96@gmail.com>
2017-02-07 14:25:22 -0800

    Completed Bowling Game Kata

---
+++ BowlingGameKata/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BowlingGameKata
+{
+    public class Game
+    {
+        private int _scoreTotal = 0;
+        private int[] _rolls = new int[21];
+        private int _currentRole = 0;
+
+        public void Roll(int pins)
+        {
+            _scoreTotal += pins;
+            _rolls[_currentRole++] = pins;
+        }
+
+        public int Score()
+        {
+            _scoreTotal = 0;
+            int frameIndex = 0;
+            for(int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    _scoreTotal += 10 + _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    _scoreTotal += 10 + _rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    _scoreTotal += _rolls[frameIndex] + _rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+            return _scoreTotal;
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return _rolls[frameIndex] == 10;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
+        }
+    }
+}
---
+++ BowlingGameTest/BownlingGameTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using BowlingGameKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Shouldly;
+using Assert = NUnit.Framework.Assert;
+
+namespace BowlingGameTest
+{
+    [TestFixture]
+    public class BownlingGameTest
+    {
+        private Game game;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            game = new Game();
+        }
+
+        [Test]
+        public void TestGutterGame()
+        {
+            RollMany(20, 0);
+            game.Score().ShouldBe(0);
+        }
+
+        private void RollMany(int numRolls, int pins)
+        {
+            for (int index = 0; index < numRolls; index++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        [Test]
+        public void TestAllOnes()
+        {
+            RollMany(20, 1);
+            game.Score().ShouldBe(20);
+        }
+
+        [Test]
+        public void TestOneSpare()
+        {
+            RollSpare();
+            game.Roll(3);
+            RollMany(17, 0);
+            game.Score().ShouldBe(16);
+        }
+
+        [Test]
+        public void TestOneStrike()
+        {
+            RollStrike();
+            game.Roll(3);
+            game.Roll(4);
+            RollMany(16, 0);
+            game.Score().ShouldBe(24);
+        }
+
+        [Test]
+        public void TestPerfectGame()
+        {
+            RollMany(12, 10);
+            game.Score().ShouldBe(300);
+        }
+
+        [TestCase(12, 10, ExpectedResult = 300)]
+        public int TestMany(int rolls, int pins)
+        {
+            RollMany(rolls, pins);
+            return game.Score();
+        }
+
+        private void RollStrike()
+        {
+            game.Roll(10);
+        }
+
+        private void RollSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+        }
+
+        
+    }
+}
